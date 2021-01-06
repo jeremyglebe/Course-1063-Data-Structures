a8984ff2c4a058496825a70b9f25c4bca98938be
Jeremy Glebe <jeremyglebe@gmail.com>
2021-01-06 14:01:11 -0600

    updated minor 1 to better match module 1 topics

--- minor/mi1/starter.cpp
+++ minor/mi1/starter.cpp
@@ -1,7 +1,11 @@
 // Convenient way to use special characters, if your terminal supports it
-#define KNIGHT "♞ "
-#define QUEEN "♕ "
-#define DARK_BLOCK "██"
+#define KING "♚"
+#define QUEEN "♛"
+#define BISHOP "♝"
+#define KNIGHT "♞"
+#define ROOK "♜"
+#define PAWN "♟︎"
+#define DARK_BLOCK "▓▓"
 #define LIGHT_BLOCK "░░"
 
 #include <iostream>
@@ -9,24 +13,9 @@
 
 using namespace std;
 
-struct ChessPiece{
-    // Must include symbol (string, char?), row, column
-};
-struct ChessBoard{
-    // Must include a 2D char array and a ChessPiece
-};
-// Functions must be prototyped/forward declared
-void printBoard(ChessBoard game);
-ChessPiece newPiece(string symbol, int row, int col);
+// Functions must be prototyped
+string promptUser();
+void readFile(string filepath, char board[8][8]);
+void printBoard(char board[8][8]);
 
-// If your structs and functions are made correctly,
-// you will NOT need to modify this main function
-int main() {
-    ChessBoard board;
-    // Board with a queen
-    board.piece = newPiece(QUEEN, 4, 3);
-    printBoard(board);
-    // Board with a knight
-    board.piece = newPiece(KNIGHT, 1, 2);
-    printBoard(board);
-}
+int main() { }
